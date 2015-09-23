da5f3198f3b233bbf1171599ba1fcbce477bdbfb
ALIZA STONE <amstone326@gmail.com>
2015-09-23 13:59:00 -0400

    successfully creating new remote branch with updated translations file

--- update_translations.py
+++ update_translations.py
@@ -3,58 +3,108 @@ import os
 import deploy_config as conf
 import xml.etree.ElementTree as ET
 
-# relative path to the messages_default.txt file within the javarosa repo
-javarosa_relative_path = './j2me/shared-resources/resources'
+# name of the master hq translations file to be updated
+hq_translations_filename = 'messages_en-2.txt'
 
-# relative path to the messages_cc_default.txt file within the commcare repo
-commcare_relative_path = './application/resources'
+# relative path to the subfolder within the javarosa repo containing the messages_default.txt file
+javarosa_subfolder = './j2me/shared-resources/resources'
 
-# relative path to the messages_ccodk_default.txt file within the commcare-odk repo
-ccodk_default_relative_path = './app/assets/locales'
+# relative path to the subfolder within the commcare repo containing the messages_cc_default.txt file
+commcare_subfolder = './application/resources'
 
-# relative path to the strings.xml file within the commcare-odk repo
-ccodk_strings_relative_path = './app/res/values'
+# relative path to the subfolder within the commcare-odk repo containing the messages_ccodk_default.txt file
+ccodk_messages_subfolder = './app/assets/locales'
+
+# relative path to the subfolder within the commcare-odk repo containing the strings.xml file
+ccodk_strings_subfolder = './app/res/values'
+
+javarosa_repo = 'javarosa'
+commcare_repo = 'commcare'
+commcare_odk_repo = 'commcare-odk'
+translations_repo = 'commcare-translations'
+
+javarosa_filename = 'messages_default.txt'
+commcare_filename = 'messages_cc_default.txt'
+ccodk_messages_filename = 'messages_ccodk_default.txt'
+ccodk_strings_filename = 'strings.xml'
+
+javarosa_header_string = '# *** messages_default.txt ***'
+commcare_header_string = '# *** messages_cc_default.txt ***'
+ccodk_messages_header_string = '# *** messages_ccodk_default.txt ***'
+ccodk_strings_header_string = '# *** strings.xml ***'
+
+header_strings = [javarosa_header_string, commcare_header_string, ccodk_messages_header_string, ccodk_strings_header_string]
 
 
-def update_translations():
-    new_javarosa_text = get_updated_translations('javarosa', javarosa_relative_path, 'messages_default.txt')
-    new_commcare_text = get_updated_translations('commcare', commcare_relative_path, 'messages_cc_default.txt')
-    new_ccodk_text = get_updated_translations('commcare-odk', ccodk_default_relative_path, 'messages_ccodk_default.txt')
+def update_translations(new_version_number):
+    new_javarosa_text = get_updated_translations(javarosa_repo, javarosa_subfolder, javarosa_filename)
+    new_commcare_text = get_updated_translations(commcare_repo, commcare_subfolder, commcare_filename)
+    new_ccodk_text = get_updated_translations(commcare_odk_repo, ccodk_messages_subfolder, ccodk_messages_filename)
     new_strings_text = get_updated_strings_block()
+    new_text_blocks = [new_javarosa_text, new_commcare_text, new_ccodk_text, new_strings_text]
+
+    new_branch_name = checkout_new_branch(new_version_number)
+    backup_old_translations_file()
+    create_updated_translations_file(new_text_blocks)
+    commit_and_push_new_branch(new_version_number, new_branch_name)
+
+
+def checkout_new_branch(new_version_number):
+    chdir_repo(translations_repo)
+    subprocess.call('git checkout master', shell=True)
+    subprocess.call('git pull origin master', shell=True)
+    new_branch_name = '{}_release_additions'.format(new_version_number)
+    subprocess.call('git checkout -b {}'.format(new_branch_name), shell=True)
+    return new_branch_name
+
+
+def backup_old_translations_file():
+    subprocess.call('mv {0} {1}'.format(hq_translations_filename, hq_translations_filename + '.bak'), shell=True)
+
+
+def create_updated_translations_file(new_text_blocks):
+    subprocess.call('touch {}'.format(hq_translations_filename), shell=True)
+    f = open(hq_translations_filename, 'w')
+    for i in range(4):
+        f.write(header_strings[i] + '\n\n')
+        f.write(new_text_blocks[i] + '\n\n')
+
+
+def commit_and_push_new_branch(new_version_number, new_branch_name):
+    subprocess.call('git add {}'.format(hq_translations_filename), shell=True)
+    subprocess.call("git commit -m '{}'".format('Auto-commit: Update translations file for CommCare release ' + new_version_number),
+                    shell=True)
+    subprocess.call('git push origin {}'.format(new_branch_name), shell=True)
 
 
 def get_updated_translations(repo, relative_path, filename):
     chdir_repo(repo)
     os.chdir(relative_path)
     f = open(filename, 'r')
-    return f.read()
+    return f.read().strip()
 
 
-# None -> String
 def get_updated_strings_block():
-    chdir_repo('commcare-odk')
-    os.chdir(ccodk_strings_relative_path)
-    tree = ET.parse('strings.xml')
+    chdir_repo(commcare_odk_repo)
+    os.chdir(ccodk_strings_subfolder)
+    tree = ET.parse(ccodk_strings_filename)
     resources = tree.getroot()
     string_list = []
     for string in resources.findall('string'):
         name = string.get('name')
         value = string.text
-        if name is not None and value is not None:
+        if name is not None and value is not None and name != 'aboutdialog':
             string_list.append(name + '=' + value + '\n')
-    print("".join(string_list))
+    return "".join(string_list)
 
 
 
-
-# String -> None
 def chdir_repo(repo):
     os.chdir(os.path.join(conf.BASE_DIR, repo))
 
 
-# None -> None
 def chdir_base():
     os.chdir(conf.BASE_DIR)
 
 
-update_translations()
+update_translations('2.23')
