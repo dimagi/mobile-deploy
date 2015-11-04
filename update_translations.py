4b0255e4fcbd0ecee0e8892150095f452d59f3d2
Phillip Mates <pmates@dimagi.com>
2015-11-04 12:20:50 -0500

    pep8 the update_translations

--- update_translations.py
+++ update_translations.py
@@ -7,16 +7,20 @@ import re
 # name of the master hq translations file to be updated
 hq_translations_filename = 'messages_en-2.txt'
 
-# relative path to the subfolder within the javarosa repo containing the messages_default.txt file
+# relative path to the subfolder within the javarosa repo containing the
+# messages_default.txt file
 javarosa_subfolder = './j2me/shared-resources/resources'
 
-# relative path to the subfolder within the commcare repo containing the messages_cc_default.txt file
+# relative path to the subfolder within the commcare repo containing the
+# messages_cc_default.txt file
 commcare_subfolder = './application/resources'
 
-# relative path to the subfolder within the commcare-odk repo containing the messages_ccodk_default.txt file
+# relative path to the subfolder within the commcare-odk repo containing the
+# messages_ccodk_default.txt file
 ccodk_messages_subfolder = './app/assets/locales'
 
-# relative path to the subfolder within the commcare-odk repo containing the strings.xml file
+# relative path to the subfolder within the commcare-odk repo containing the
+# strings.xml file
 ccodk_strings_subfolder = './app/res/values'
 
 javarosa_repo = 'javarosa'
@@ -29,21 +33,31 @@ commcare_filename = 'messages_cc_default.txt'
 ccodk_messages_filename = 'messages_ccodk_default.txt'
 ccodk_strings_filename = 'strings.xml'
 
-all_filenames = [javarosa_filename, commcare_filename, ccodk_messages_filename, ccodk_strings_filename]
-all_repos = [javarosa_repo, commcare_repo, commcare_odk_repo, translations_repo]
+all_filenames = [javarosa_filename, commcare_filename,
+                 ccodk_messages_filename, ccodk_strings_filename]
+all_repos = [javarosa_repo, commcare_repo,
+             commcare_odk_repo, translations_repo]
 
-strings_namespace = '{http://strings_namespace}'
+namespace = '{http://strings_namespace}'
 github_url = 'https://github.com/dimagi/commcare-translations/compare/'
 
 
 def update_translations(new_version_number):
     if util.unstaged_changes_present(all_repos):
-        raise Exception("One of your repositories has un-staged changes, please stash them and try again")
-    new_javarosa_text = get_updated_translations(javarosa_repo, javarosa_subfolder, javarosa_filename)
-    new_commcare_text = get_updated_translations(commcare_repo, commcare_subfolder, commcare_filename)
-    new_ccodk_text = get_updated_translations(commcare_odk_repo, ccodk_messages_subfolder, ccodk_messages_filename)
+        raise Exception("One of your repositories has un-staged changes, " +
+                        "please stash them and try again")
+    new_javarosa_text = get_updated_translations(javarosa_repo,
+                                                 javarosa_subfolder,
+                                                 javarosa_filename)
+    new_commcare_text = get_updated_translations(commcare_repo,
+                                                 commcare_subfolder,
+                                                 commcare_filename)
+    new_ccodk_text = get_updated_translations(commcare_odk_repo,
+                                              ccodk_messages_subfolder,
+                                              ccodk_messages_filename)
     new_strings_text = get_updated_strings_block()
-    new_text_blocks = [new_javarosa_text, new_commcare_text, new_ccodk_text, new_strings_text]
+    new_text_blocks = [new_javarosa_text, new_commcare_text,
+                       new_ccodk_text, new_strings_text]
 
     new_branch_name = checkout_new_translations_branch(new_version_number)
     backup_old_translations_file()
@@ -64,9 +78,11 @@ def backup_old_translations_file():
     os.rename(hq_translations_filename, hq_translations_filename + '.bak')
 
 
-# Write all of the updated text blocks from the 4 mobile translations files to the master hq translations file, with
-# headers for each section
 def create_updated_translations_file(new_text_blocks):
+    """
+    Write all of the updated text blocks from the 4 mobile translations files
+    to the master hq translations file, with headers for each section
+    """
     header_prefix = '# *** '
     header_suffix = ' ***'
     with open(hq_translations_filename, 'w', encoding='utf-8') as f:
@@ -79,18 +95,24 @@ def create_updated_translations_file(new_text_blocks):
                 f.write('\n\n')
 
 
-def commit_and_push_new_branch(new_version_number, new_branch_name):
+def commit_and_push_new_branch(new_version_number, new_branch):
     subprocess.call('git add {}'.format(hq_translations_filename), shell=True)
-    commit_message = 'Auto-commit: Update translations file for CommCare release ' + new_version_number
+    commit_message = ('Auto-commit: Update translations for ' +
+                      'CommCare release {}').format(new_version_number)
     subprocess.call("git commit -m '{}'".format(commit_message),
                     shell=True)
-    subprocess.call('git push origin {}'.format(new_branch_name), shell=True)
-    print('An updated translations file has been pushed to GitHub as branch {0}. To create a PR out of this '
-          'branch, you can go directly to {1}'.format(new_branch_name, github_url + new_branch_name))
+    subprocess.call('git push origin {}'.format(new_branch), shell=True)
+    pr_url = '{}{}'.format(github_url, new_branch)
+    print(('An updated translations file has been pushed to GitHub ' +
+           'as branch {0}. To create a PR out of this ' +
+           'branch, you can go directly to {1}').format(new_branch, pr_url))
 
 
-# Return a string containing the updated text that should go into the master hq translations file from the given file
 def get_updated_translations(repo, relative_path, filename):
+    """
+    Return a string containing the updated text that should go into the master
+    hq translations file from the given file
+    """
     util.chdir_repo(repo)
     subprocess.call('git checkout master', shell=True)
     os.chdir(relative_path)
@@ -98,10 +120,13 @@ def get_updated_translations(repo, relative_path, filename):
         return f.read().strip()
 
 
-# Return a string containing the updated text that should go into the master hq translations file from the strings.xml
-# file in the mobile codebase. Because it is an xml file instead of a plain text file, the extraction needs to be done
-# in a different way from all of the other files
 def get_updated_strings_block():
+    """
+    Return a string containing the updated text that should go into the master
+    hq translations file from the strings.xml file in the mobile codebase.
+    Because it is an xml file instead of a plain text file, the extraction
+    needs to be done in a different way from all of the other files
+    """
     util.chdir_repo(commcare_odk_repo)
     subprocess.call('git checkout master', shell=True)
     os.chdir(ccodk_strings_subfolder)
@@ -109,7 +134,7 @@ def get_updated_strings_block():
     resources = tree.getroot()
     string_list = []
     for string in resources.findall('string'):
-        translatable_value = string.get('{}translatable'.format(strings_namespace))
+        translatable_value = string.get('{}translatable'.format(namespace))
         if translatable_value == 'true':
             name = string.get('name')
             value = string.text
@@ -132,7 +157,8 @@ def replace_helper(match_obj):
     else:
         # Otherwise, there is some number sitting in between '%' and '$s'
         index = text[1:-2]
-        # Decrement this by 1 because HQ's indexing starts at 0 and ours starts at 1
+        # Decrement this by 1 because HQ's indexing starts at 0 and ours starts
+        # at 1
         new_index = int(index) - 1
     return '${' + str(new_index) + '}'
 
